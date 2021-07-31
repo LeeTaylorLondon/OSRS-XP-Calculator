91e92222d51b19ce2fdc49105333e50448de34af
Lee <lees_taylor_18@yahoo.com>
2021-07-31 23:16:28 +0100

    Imported project & updated README.md with notes Added .gitignore to ignore .idea folder

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # OSRS-XP-Calculator
-Small side project. Input current level or xp, target level, name of skill (optional), XP per hour, and hours per day. Number of hours and days needed to reach target level is outputted. 
+Small side project. Input current level or xp, target level, name of skill (optional), XP per hour, and hours per day.
+Number of hours and days needed to reach target level is outputted.  
+
+There are examples of using the provided functions in the main.py file. The formulae to calculate XP for a level 
+is from the OSRS wiki. 
+
+I made this to calculate the number of days I needed to stay in nightmare zone. This works with all other skills 
+not just combat skills. 
+
---
+++ main.py
@@ -0,0 +1,59 @@
+# Author: Lee Taylor
+import math
+
+
+def calc_xp(L:int) -> int:
+    """ Return xp difference between L and L-1 """
+    return math.ceil(0.25 * math.floor((L-1) + (300 * 2 ** ((L-1)/7))))
+
+# Calc. XP, time in hours and days, with approx. data and time from levels X to Y
+# Where X,Y = user input
+
+def lvl_to_lvl(start_lvl:int=1, end_lvl:int=99, lvl_name:str="", xphr:int=80000, hrsd:float=15.0):
+    """ start_lvl: starting level to calculate from
+          end_lvl: target level to acquire
+          xphr: xp gained per hour
+          hrsd: hours played per day
+    """
+    sum_xp = 0
+    for lvl in range(start_lvl + 1, end_lvl + 1):
+        sum_xp += calc_xp(lvl)
+    hours = round(sum_xp / xphr, 1)
+    days = round(hours/hrsd, 1)
+    print(f"From level [{start_lvl}] to [{end_lvl}] [{lvl_name}]\n\
+Hours: [{hours}], days: [{days}], XP Remaining: [{sum_xp}]\n")
+    return sum_xp, hours, days
+
+def xp_to_lvl(start_xp:int=1, end_lvl:int=99, lvl_name:str="", xphr:int=80000, hrsd:float=15.0):
+    """ start_xp: starting xp to calculate from
+          end_lvl: target level to acquire
+          xphr: xp gained per hour
+          hrsd: hours played per day
+    """
+    needed_xp = 0
+    for L in range(1, end_lvl+1):
+        needed_xp += calc_xp(L)
+    remaining_xp = needed_xp - start_xp
+    hours = round(remaining_xp / xphr, 1)
+    days = round(hours/hrsd, 1)
+    print(f"From XP [{start_xp}] to [{end_lvl}] [{lvl_name}]\n\
+Hours: [{hours}], days: [{days}], XP Remaining: [{remaining_xp}]\n")
+    return remaining_xp, hours, days
+
+
+# lvl_to_lvl(85, 95, "ATK")
+# xp_to_lvl(3362605, 88, "ATK", 75000)
+# lvl_to_lvl(85, 97, "ATK", 75000)
+# xp_to_lvl(3663605, 87, "ATK", 75000)
+# xp_to_lvl(2165605, 90, "DEF", 73000)
+# lvl_to_lvl(80, 95, "DEF")
+xp_to_lvl(5446859, 95, "ATK", 81000)
+xp_to_lvl(2196594, 90, "DEF", 75000)
+xp_to_lvl(10176639, 99, "HP", 25000, 15)
+xp_to_lvl(5446859, 99, "ATK", 81000)
+
+# lvl_to_lvl(85, 91, "Thieving", 195000, 3)
+xp_to_lvl(6438100, 95, "Thieving", 228000, 2)
+lvl_to_lvl(95, 99, "Thieving", 250000, 2)
+
+
