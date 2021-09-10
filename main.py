7f3c8a661f67ed486ea37140b8e1b134e97b8427
Lee <lees_taylor_18@yahoo.com>
2021-09-10 17:22:08 +0100

    Updated functions to print detailed rates of XP/HR and HRS/DAY

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def lvl_to_lvl(start_lvl:int=1, end_lvl:int=99, lvl_name:str="", xphr:int=80000,
         sum_xp += calc_xp(lvl)
     hours = round(sum_xp / xphr, 1)
     days = round(hours/hrsd, 1)
-    print(f"From level [{start_lvl}] to [{end_lvl}] [{lvl_name}]\n\
+    print(f"From level [{start_lvl}] to [{end_lvl}] [{lvl_name}] @ [{xphr}/Hr] for [{hrsd}Hrs/Day]\n\
 Hours: [{hours}], days: [{days}], XP Remaining: [{sum_xp}]\n")
     return sum_xp, hours, days
 
@@ -36,7 +36,7 @@ def xp_to_lvl(start_xp:int=1, end_lvl:int=99, lvl_name:str="", xphr:int=80000, h
     remaining_xp = needed_xp - start_xp
     hours = round(remaining_xp / xphr, 1)
     days = round(hours/hrsd, 1)
-    print(f"From XP [{start_xp}] to [{end_lvl}] [{lvl_name}]\n\
+    print(f"From XP [{start_xp}] to [{end_lvl}] [{lvl_name}] @ [{xphr}/Hr] for [{hrsd}Hrs/Day]\n\
 Hours: [{hours}], days: [{days}], XP Remaining: [{remaining_xp}]\n")
     return remaining_xp, hours, days
 
@@ -47,13 +47,18 @@ Hours: [{hours}], days: [{days}], XP Remaining: [{remaining_xp}]\n")
 # xp_to_lvl(3663605, 87, "ATK", 75000)
 # xp_to_lvl(2165605, 90, "DEF", 73000)
 # lvl_to_lvl(80, 95, "DEF")
-xp_to_lvl(5446859, 95, "ATK", 81000)
-xp_to_lvl(2196594, 90, "DEF", 75000)
-xp_to_lvl(10176639, 99, "HP", 25000, 15)
-xp_to_lvl(5446859, 99, "ATK", 81000)
+# xp_to_lvl(5446859, 95, "ATK", 81000)
+xp_to_lvl(2893275, 90, "DEF", 79_000, 15)
+xp_to_lvl(2893275, 90, "DEF", 79_000, 8)
+xp_to_lvl(2893275, 99, "DEF", 79_000, 15)
+xp_to_lvl(2893275, 99, "DEF", 79_000, 8)
+xp_to_lvl(10176639, 99, "HP", 26_400, 15)
+xp_to_lvl(10176639, 99, "HP", 26_400, 8)
+xp_to_lvl(7026618, 99, 'THIEVING', 220_000, 1.5)
+# xp_to_lvl(5446859, 99, "ATK", 81000)
 
 # lvl_to_lvl(85, 91, "Thieving", 195000, 3)
-xp_to_lvl(6438100, 95, "Thieving", 228000, 2)
+# xp_to_lvl(6438100, 95, "Thieving", 228000, 2)
 lvl_to_lvl(95, 99, "Thieving", 250000, 2)
 
 
